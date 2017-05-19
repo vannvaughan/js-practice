d860456ee443f040a88301ca9f29862460c46986
Brian Hudson <brianjasonhudson@gmail.com>
2017-05-19 11:12:48 -0500

    Added samples and five test problems

--- practice.js
+++ practice.js
@@ -35,3 +35,30 @@
    }
 // Check the test
    // Click on the failing test (labeled "returns the truth") to see what you produced and what it expected
+
+
+// 1. isEven
+   // Write a function called isEven, which takes in an integer as a parameter and returns true if the integer is even and false if the integer is odd.
+
+   // N.B. Return the boolean value true or false, not the string values of the words. 
+
+   function isEven( ) {
+
+   }
+
+// 2. returnSmallest
+  // Write a function called returnSmallest that takes in an array of numbers as a parameter and returns the smallest value in the array.
+
+
+// 3. addUser
+  // Write a function called createUser, which takes in two parameters, an array of usernames and a new username. Create a new array which contains all of the usernames, including the new username. Return the new array.
+
+// 4. reverseName
+   // Were you ever obsessed with saying your name backwards as a kid? I was for some reason. Write a function called reverseName which takes in a name and returns the reverse of the name. HINT: You can build a string through concatenation: 
+   // var newString = oldString + 'a';
+   // OR:
+   // oldString += 'a';
+
+// 5. countVowels 
+   // Write a function called countVowels that takes in a string and returns the number of vowels in the string. 
+
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,4 +1,4 @@
-describe("Sample Task", function() {
+describe("speakTheTruth", function() {
    it("exists", function() {
       expect(speakTheTruth).toEqual(jasmine.any(Function));
    })
@@ -7,11 +7,78 @@ describe("Sample Task", function() {
    })
 })
 
-describe("Speak Falsehood", function() {
+describe("speakFalsehood", function() {
    it("exists", function() {
       expect(speakFalsehood).toEqual(jasmine.any(Function));
    })
-   it("returns the truth", function(){
+   it("returns a falsehood", function(){
       expect(speakFalsehood()).toBe("Shea is the best")
    })
 })
+
+describe("isEven", function() {
+   it("exists", function() {
+      expect(isEven).toEqual(jasmine.any(Function));
+   })
+   it("returns true for even numbers", function(){
+      expect(isEven(44)).toBe(true)
+      expect(isEven(2)).toBe(true)
+   })
+   it("returns false for odd numbers", function(){
+      expect(isEven(3)).toBe(false)
+      expect(isEven(1)).toBe(false)
+   })
+})
+
+describe("returnSmallest", function() {
+   it("exists", function() {
+      expect(returnSmallest).toEqual(jasmine.any(Function));
+   })
+   it("returns the smallest value", function(){
+      expect(returnSmallest([1, 2, 3, 4])).toBe(1)
+      expect(returnSmallest([601, 599, 588, 3])).toBe(3)
+   })
+})
+
+describe("addUser", function() {
+   var names = ['brian', 'shea']
+   it("exists", function() {
+      expect(addUser).toEqual(jasmine.any(Function));
+   })
+   it("returns an array", function(){
+      expect(addUser(names)).toEqual(jasmine.any(Array))
+   })
+   it("returns a new array", function(){
+      expect(addUser(names, 'steven')).not.toBe(names)
+   })
+   it("returns all usernames", function() {
+      expect(addUser(names, 'steven').length).toEqual(names.length + 1)
+      expect(addUser(names, 'steven')).toEqual(['brian', 'shea', 'steven'])
+   })
+})
+
+describe("reverseName", function() {
+   it("exists", function() {
+      expect(reverseName).toEqual(jasmine.any(Function));
+   })
+   it("returns a reversed string", function(){
+      expect(reverseName("brian")).toEqual("nairb")
+      expect(reverseName("shea")).toEqual("aehs")
+   })
+})
+
+describe("countVowels", function() {
+   it("exists", function() {
+      expect(countVowels).toEqual(jasmine.any(Function));
+   })
+   it("returns the number of vowels in a string", function(){
+      expect(countVowels("brian")).toBe(2)
+      expect(countVowels("shea")).toBe(2)
+      expect(countVowels("creighton")).toBe(3)
+   })
+})
+
+
+
+
+
